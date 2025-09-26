f1a920281bd9f67d1e152a05e8f0ae886a0481ff
Martin Ferrero-Thompson <martin.ferrero.thompson@gmail.com>
2025-09-26 12:58:03 +0200

    FIX: Re-sync all dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   integrations: [tailwind(), react()],
-  viewTransitions: true
+  viewTransitions: true,
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
