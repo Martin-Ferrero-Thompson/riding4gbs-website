57881cb49bbe04408dfa27dd001b1c677b6692ec
Martin Ferrero-Thompson <martin.ferrero.thompson@gmail.com>
2025-09-29 10:09:12 +0200

    Added - style guide tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import tailwindcss from 'tailwindcss';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -7,7 +9,26 @@ export default {
         'sans': ['Lato', 'sans-serif'],
         'display': ['Montserrat', 'sans-serif'],
       },
-    }, 
-  },   
+      colors: {
+        'brand-blue': '#2563eb', // This is blue-600
+        'brand-teal': '#14b8a6', // This is teal-500
+      },
+      animation: {
+        'slide-in-left': 'slide-in-from-left 1s ease-out forwards',
+      },
+      keyframes: {
+        'slide-in-from-left': {
+          'from': { 
+            transform: 'translateX(-50px)',
+            opacity: '0',
+          },
+          'to': { 
+            transform: 'translateX(0)',
+            opacity: '1',
+          },
+        },
+      },
+    },
+  },
   plugins: [],
 }
